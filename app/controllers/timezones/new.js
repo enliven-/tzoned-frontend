f89e9fc9036f02ae691606b83a8d4de830f39cc8
Viksit Arora <viksit@mit.edu>
2016-01-21 15:30:17 +0530

    adds addTimezone action to timezones/new controller

--- app/controllers/timezones/new.js
+++ app/controllers/timezones/new.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions : {
+    addTimezone() {
+      var name = this.get('name');
+      var abbr = this.get('abbr');
+      var gmt_difference = this.get('gmt_difference');
+      console.log(name);
+      console.log(abbr);
+      console.log(gmt_difference);
+    }
+  }
 });
