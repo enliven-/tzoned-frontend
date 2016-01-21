c3976ef5176ded8db1fa01d9bf39c872897b11cc
Viksit Arora <viksit@mit.edu>
2016-01-21 12:31:02 +0530

    generates timezones/new route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('timezones');
+  this.route('timezones', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
