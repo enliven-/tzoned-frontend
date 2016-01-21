91c9243482e7395ac07160fd9acea7d70deaa916
Viksit Arora <viksit@mit.edu>
2016-01-21 15:45:30 +0530

    updates addTimezone action to create newTimezone from fields

--- app/controllers/timezones/new.js
+++ app/controllers/timezones/new.js
@@ -1,14 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  
   actions : {
+
     addTimezone() {
       var name = this.get('name');
       var abbr = this.get('abbr');
       var gmt_difference = this.get('gmt_difference');
-      console.log(name);
-      console.log(abbr);
-      console.log(gmt_difference);
+
+      // create new timezone in memory
+      var newTimezone = this.store.createRecord('timezone', {
+        name : name,
+        abbr : abbr,
+        gmt_difference : gmt_difference
+      });
+
+      console.log(newTimezone);
     }
   }
 });
--- app/models/timezone.js
+++ app/models/timezone.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name  : DS.attr('string'),
+  abbr  : DS.attr('string'),
+  gmt_difference: DS.attr('string')
 });
