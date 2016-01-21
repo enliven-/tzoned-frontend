9cd27c98295ed8f620c3ec49d96f2f9682b57e25
Viksit Arora <viksit@mit.edu>
2016-01-21 19:37:53 +0530

    adds delte action to timezones

--- app/controllers/timezones.js
+++ app/controllers/timezones.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions : {
+    deleteTimezone(id) {
+      this.store.findRecord('timezone', id).then(function(timezone) {
+        timezone.deleteRecord();
+        timezone.save();
+      })
+    }
+  }
 });
