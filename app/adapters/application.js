5973f341db0fa359aa0ead72fc436e0d77156647
Viksit Arora <viksit@mit.edu>
2016-01-22 17:52:54 +0530

    modifies adapter to include headers and namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,13 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  host: 'http://tzoned-api.dev/users/3',
+  host: 'http://tzoned-api.dev',
+
+  headers: { 'Authorization' : 'Ly9ojjBsPqj5skdHaMU8' },
+
+  namespace : Ember.computed(function() {
+    return 'users/1';
+  }).volatile(),
 
   shouldReloadAll(store, snapshot) { return true; },
 
