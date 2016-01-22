edbf2e3a085a12c801d7b8807ab684c48efd024b
Viksit Arora <viksit@mit.edu>
2016-01-23 02:49:11 +0530

    minor cleanup

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,18 +3,13 @@ import Ember from 'ember';
 const { service } = Ember.inject;
 
 export default Ember.Controller.extend({
-  // session: Ember.inject.service('session'),
   session: service('session'),
 
   actions: {
     authenticate() {
-      console.log('here')
       let { identification, password } = this.getProperties('identification', 'password');
-      console.log(identification);
-      console.log(password);
       this.get('session').authenticate('authenticator:devise', identification, password).then(() => {
-        // alert('Success! Click the top link!');
-        console.log( this.get('session') );
+        alert('Success! Click the top link!');
       }, (err) => {
         alert('Error obtaining token: ' + err.responseText);
       })
