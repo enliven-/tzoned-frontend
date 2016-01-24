c1dc73958d7f851364a5d0263a5984e826f8048a
Viksit Arora <viksit@mit.edu>
2016-01-24 21:38:17 +0530

    makes users route authenticated as well

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,13 +1,11 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
-  beforeModel() {
-    this.store.unloadAll('user');
-  },
-  
   model() {
     return this.store.findAll('user');
   }
+  
 });
